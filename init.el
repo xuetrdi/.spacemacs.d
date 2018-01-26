64ff0a5ed07b5a01545c0083956c4f7ece20949a
xuetrdi <xuetr_di@icloud.com>
2018-01-27 00:09:32 +0800

    modify some conf.

--- init.el
+++ init.el
@@ -112,6 +112,13 @@ values."
                                     ivy-purpose
                                     spacemacs-purpose-popwin
                                     window-purpose
+                                    org-plus-contrib
+                                    org-brain
+                                    org-present
+                                    orgit
+                                    info+
+                                    help-fns+
+                                    hide-comnt
                                     )
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
@@ -373,16 +380,16 @@ you should place your code here."
   ;;UTF-8
   (set-language-environment "UTF-8")
   (set-default-coding-systems 'utf-8)
-  (set-buffer-file-coding-system 'utf-8-unix)
-  (set-clipboard-coding-system 'utf-8-unix)
-  (set-file-name-coding-system 'utf-8-unix)
-  (set-keyboard-coding-system 'utf-8-unix)
-  (set-next-selection-coding-system 'utf-8-unix)
-  (set-selection-coding-system 'utf-8-unix)
-  (set-terminal-coding-system 'utf-8-unix)
+  ;;(set-buffer-file-coding-system 'utf-8-unix)
+  ;;(set-clipboard-coding-system 'utf-8-unix)
+  ;;(set-file-name-coding-system 'utf-8-unix)
+  ;;(set-keyboard-coding-system 'utf-8-unix)
+  ;;(set-next-selection-coding-system 'utf-8-unix)
+  ;;(set-selection-coding-system 'utf-8-unix)
+  ;;(set-terminal-coding-system 'utf-8-unix)
   (setq locale-coding-system 'utf-8)
   (prefer-coding-system 'utf-8)
-  (setq powerline-default-separator 'utf-8)
+  ;;(setq powerline-default-separator 'utf-8)
 
   ;; go-complete
   (require 'go-autocomplete)
@@ -413,3 +420,23 @@ you should place your code here."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((((class color) (min-colors 257)) (:foreground "#F8F8F2" :background "#272822")) (((class color) (min-colors 89)) (:foreground "#F5F5F5" :background "#1B1E1C")))))
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-brain youdao-dictionary names chinese-word-at-point go-autocomplete flycheck-gometalinter go-guru go-eldoc company-go go-mode company-web web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode haml-mode emmet-mode web-completion-data ein skewer-mode request-deferred websocket deferred js2-mode f s dash monokai-theme mmm-mode markdown-toc markdown-mode gh-md yapfify xterm-color unfill smeargle shell-pop pyvenv pytest pyenv-mode py-isort pip-requirements orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-download mwim multi-term magit-gitflow live-py-mode hy-mode htmlize gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link fuzzy flyspell-correct-ivy flyspell-correct flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit with-editor eshell-z eshell-prompt-extras esh-help cython-mode company-statistics company-anaconda company auto-yasnippet yasnippet auto-dictionary anaconda-mode pythonic ac-ispell auto-complete ws-butler winum which-key wgrep volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline smex restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint ivy-hydra info+ indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-make helm helm-core google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump popup diminish define-word counsel-projectile projectile pkg-info epl counsel swiper ivy column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed async aggressive-indent adaptive-wrap ace-window ace-link avy evil-unimpaired))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 257)) (:foreground "#F8F8F2" :background "#272822")) (((class color) (min-colors 89)) (:foreground "#F5F5F5" :background "#1B1E1C")))))
+)
