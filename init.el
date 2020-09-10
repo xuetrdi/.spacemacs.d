d3cd7bda75d8891b2d608ea455327132cb8cb5e6
xuetrdi <xuetr_di@icloud.com>
2020-09-10 13:57:33 +0800

    add ropemacs

--- init.el
+++ init.el
@@ -15,7 +15,8 @@
                       syntax-checking-enable-tooltips nil
                       :disabled-for
                       go
-                      python)
+                      python
+                      c++)
      emacs-lisp
      (git :variables
           git-magit-status-fullscreen t)
@@ -26,81 +27,54 @@
             latex-enable-auto-fill t
             latex-enable-folding t
             latex-enable-magic t)
-     (shell :variables shell-default-shell 'zsh)
-     (docker :variables
-             docker-dockerfile-backend 'lsp)
      dap
      lsp
      (python :variables
              python-backend 'lsp
+             python-lsp-server 'mspyls
+             python-lsp-git-root "/usr/local/build/python-language-server"
              python-sort-imports-on-save nil
              python-formatter 'yapf
              python-tab-width 2
              python-indent-offset 2)
-     scheme
-     (clojure :variables
-              clojure-enable-fancify-symbols t)
-     (scala :variables
-            scala-indent:use-javadoc-style t
-            scala-enable-eldoc t
-            scala-auto-insert-asterisk-in-comments t
-            scala-use-unicode-arrows t
-            scala-auto-start-ensime t)
      imenu-list
-     (javascript :variables
-                 js2-basic-offset 2
-                 js-indent-level 2
-                 javascript-import-tool 'import-js
-                 javascript-backend 'lsp
-                 javascript-fmt-tool 'prettier
-                 javascript-fmt-on-save t
-                 javascript-repl `nodejs)
-     ;; (typescript :variables
-     ;;             typescript-fmt-on-save t
-     ;;             typescript-fmt-tool 'typescript-formatter
-     ;;             typescript-linter 'tslint)
-     gpu
      (c-c++ :variables
-            c-c++-backend 'lsp-ccls
+            ;; c-c++-backend 'lsp-ccls
             c-c++-default-mode-for-headers 'c++-mode
             c-c++-enable-google-style t
             c-c++-enable-google-newline t
-            c-c++-lsp-executable (file-truename "/usr/local/bin/ccls")
+            ;; c-c++-lsp-executable (file-truename "/usr/local/bin/ccls")
             c-c++-lsp-sem-highlight-rainbow t
             c-c++-lsp-sem-highlight-method 'font-lock
-            c-c++-lsp-cache-dir "/tmp/lsp-ccls"
-            ;; c-c++-enable-clang-support t
+            ;; c-c++-lsp-cache-dir "/tmp/lsp-ccls"
             )
      (cmake :variables cmake-enable-cmake-ide-support t)
-     (rust :variables
-           rust-backend 'lsp)
      swift
-     ;; (julia :variables
-     ;;        julia-mode-enable-ess nil
-     ;;        julia-mode-enable-lsp t)
-     ;; themes-megapack
-     ;; yaml
+     rust
      protobuf
      (go :variables
          go-backend 'lsp
          go-tab-width 2
          gofmt-command "goimports"
          gofmt-before-save t
+         go-use-golangci-lint t
          godoc-at-point-function 'godoc-gogetdoc)
-     (chinese :package youdao-dictionary fcitx
-              :variables chinese-enable-fcitx nil
-              chinese-enable-youdao-dict t)
+     ;; (chinese :package youdao-dictionary fcitx
+     ;;          :variables chinese-enable-fcitx nil
+     ;;          chinese-enable-youdao-dict t)
    )
    dotspacemacs-additional-packages '(
-                                      go-autocomplete
                                       all-the-icons
                                       all-the-icons-dired
                                       all-the-icons-ivy
                                       sr-speedbar
+                                      company-lsp
                                       auctex
-                                      xah-math-input
+                                      ;; xah-math-input
                                       lsp-sourcekit
-                                      eglot
+                                      emr
+                                      ;; eglot
+                                      ;; valign-mode
                                       )
    dotspacemacs-frozen-packages '()
    dotspacemacs-excluded-packages '(vi-tilde-fringe
@@ -108,37 +82,37 @@
                                     chinese-pyim
                                     counsel-projectile
                                     company-quickhelp
-                                    ;; evil-args
-                                    ;; evil-ediff
-                                    ;; evil-mc
-                                    ;; evil-exchange
-                                    ;; evil-unimpaired
-                                    ;; evil-indent-plus
-                                    ;; evil-escape
-                                    ;; evil-lisp-state
+                                    evil-args
+                                    evil-ediff
+                                    evil-mc
+                                    evil-exchange
+                                    evil-unimpaired
+                                    evil-indent-plus
+                                    evil-escape
+                                    evil-lisp-state
                                     helm-flyspell
                                     helm-swoop
                                     helm-purpose
                                     helm-flyspell
-                                    ;; helm-spacemacs-help
+                                    helm-spacemacs-help
                                     helm-themes
                                     helm-c-yasnippet
-                                    ;; org-projectile
-                                    ;; org-download
-                                    ;; org-timer
-                                    ;; org-repo-todo
+                                    org-projectile
+                                    org-download
+                                    org-timer
+                                    org-repo-todo
                                     ;; org-plus-contrib
-                                    ;; org-brain
-                                    ;; org-present
-                                    ;; orgit
+                                    org-brain
+                                    org-present
+                                    orgit
                                     magit-gh-pulls
                                     magit-gitflow
                                     magithub
                                     git-gutter
                                     git-gutter-fringe
                                     gh-md
-                                    ;; smooth-scrolling
-                                    ;; eyebrowse
+                                    smooth-scrolling
+                                    eyebrowse
                                     auto-dictionary
                                     flyspell-correct-helm
                                     ivy-purpose
@@ -167,20 +141,6 @@
    dotspacemacs-themes '(
                          seti
                          bubbleberry
-                         badwolf
-                         cherry-blossom
-                         ir-black
-                         spolsky
-                         afternoon
-                         alect-black
-                         alect-black-alt
-                         wheatgrass
-                         junio
-                         purple-haze
-                         spolsky
-                         ir-black
-                         monokai
-                         spacemacs-dark
                          spacemacs-light)
    dotspacemacs-colorize-cursor-according-to-state t
    dotspacemacs-default-font '("Monaco"
@@ -235,7 +195,9 @@
    ))
 
 (defun dotspacemacs/user-init ()
-   (setq tramp-ssh-controlmaster-options
+  (setq byte-compile-warnings '(cl-functions))
+  (add-to-list 'load-path "~/.emacs.d/private/lisp")
+  (setq tramp-ssh-controlmaster-options
         "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
   )
 
@@ -247,34 +209,48 @@
   (prefer-coding-system 'utf-8)
   (setq powerline-default-separator 'utf-8)
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Clojure;;;;;;;;;;;;;;;;;;;;;;;
-  (setq clojure-enable-linters t)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;EMR;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (define-key prog-mode-map (kbd "M-RET") 'emr-show-refactor-menu)
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Golang;;;;;;;;;;;;;;;;;;;;;;;
-  (require 'go-autocomplete)
-  (require 'auto-complete-config)
-  (ac-config-default)
-  (setq gofmt-command "goimports")
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;PyRope;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (setq pymacs-load-path '("/usr/local/build/rope"
+                           "/usr/local/build/ropemacs"))
+  (defun load-ropemacs ()
+    "Load pymacs and ropemacs"
+    (interactive)
+    (require 'pymacs)
+    (autoload 'pymacs-load "pymacs" nil t)
+    (autoload 'pymacs-eval "pymacs" nil t)
+    (autoload 'pymacs-apply "pymacs")
+    (autoload 'pymacs-call "pymacs")
+    (autoload 'pymacs-exec "pymacs" nil t)
+    (pymacs-load "ropemacs" "rope-")
+    (global-set-key [(meta ?/)] 'rope-code-assist)
+    ;; Automatically save project python buffers before refactorings
+    (setq ropemacs-confirm-saving 'nil)
+    )
+  ;; (global-set-key "\C-xpl" 'load-ropemacs)
+  ;; (require 'pymacs)
+  ;; (pymacs-load "ropemacs" "rope-")
+  ;; (autoload 'pymacs-load "pymacs" nil t)
+  ;; (autoload 'pymacs-eval "pymacs" nil t)
+  ;; (autoload 'pymacs-apply "pymacs")
+  ;; (autoload 'pymacs-call "pymacs")
+  ;; (autoload 'pymacs-exec "pymacs" nil t)
+  ;; (global-set-key [(meta ?/)] 'rope-code-assist)
+  ;; (setq ropemacs-confirm-saving 'nil)
+  (add-hook 'python-mode-hook 'load-ropemacs)
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Clojure;;;;;;;;;;;;;;;;;;;;;;;
+  ;; (setq clojure-enable-linters t)
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Python;;;;;;;;;;;;;;;;;;;;;;;
   (setq python-shell-prompt-detect-failure-warning nil)
-  ;; (setq socks-server '("Default server" "127.0.0.1" 1080 5))
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Scala;;;;;;;;;;;;;;;;;;;;;;;;
-  (setq-default flycheck-scalastylerc "~/conf/scalastyle_config.xml")
-
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Jalia;;;;;;;;;;;;;;;;;;;;;;;;
-  ;; (require 'julia-repl)
-  ;; (setq inferior-julia-program-name "/Applications/Julia-1.0.app/Contents/Resources/julia/bin/julia")
-  ;; (add-hook 'julia-mode-hook 'julia-repl-mode)
-  ;; (add-to-list 'load-path "~/.emacs.d/private/julia-emacs")
-  ;; (require 'julia-mode)
-  ;; (add-hook 'ess-julia-mode-hook #'lsp-mode)
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Orgmode;;;;;;;;;;;;;;;;;;;;;;
   ;; orgmode preview set image size
-  (image-type-available-p 'imagemagick)
-  (setq org-image-actual-width '(400))
+  ;; (image-type-available-p 'imagemagick)
+  ;; (setq org-image-actual-width '(400))
 
   ;; 导出文本阻止下划线成下标
   (setq org-export-with-sub-superscripts nil)
@@ -322,21 +298,6 @@
       (kill-new rtn)
       rtn))
 
-  (defun org-capture-template-goto-link ()
-    (org-capture-put :target (list 'file+headline
-                                   (nth 1 (org-capture-get :target))
-                                   (org-capture-get :annotation)))
-    (org-capture-put-target-region-and-position)
-    (widen)
-    (let ((hd (nth 2 (org-capture-get :target))))
-      (goto-char (point-min))
-      (if (re-search-forward
-           (format org-complex-heading-regexp-format (regexp-quote hd)) nil t)
-          (org-end-of-subtree)
-        (goto-char (point-max))
-        (or (bolp) (insert "\n"))
-        (insert "* " hd "\n"))))
-
   ;; orgmode hotkey, such as <s
   (require 'org-tempo)
   ;; orgmode todolist
@@ -354,13 +315,13 @@
                                    (latex . t)
                                    (shell . t)
                                    (C . t)
-                                   (clojure . t)
+                                   ;; (clojure . t)
                                    ;; (java . t)
                                    (js . t)
                                    ;; (lua . t)
                                    ;; (matlab . t)
-                                   (org . t)
-                                   (scheme . t)
+                                   ;; (org . t)
+                                   ;; (scheme . t)
                                    ;; (sql . t)
                                    ;; (css . t)
                                    ;; (julia . t)
@@ -370,24 +331,9 @@
     (global-set-key (kbd "C-c c") 'org-capture)
     (setq org-default-notes-file "~/Dropbox/org/inbox.org")
     (setq org-capture-templates `(
-                                  ("t" "TODO" entry (file+headline "~/Dropbox/org/gtd.org" "Task")
-                                   "* TODO [#B] %?\n %i\n"
-                                   :empty-lines 1)
                                   ("c" "Chrome" entry (file+headline "~/Dropbox/org/link.org" "Quick notes")
                                    "* TODO [#C] %?\n %(org-mac-paste-applescript-links (org-as-mac-chrome-get-frontmost-url))\n %i\n %U"
                                    :empty-lines 1)
-	                                ("l" "Protocol Link" entry (file+headline "~/Dropbox/org/link.org" "Inbox")
-                                   "* TODO [#C] %?\n %i\n %a \n %U")
-                                  ("p" "Protocol" entry (file+headline ,(concat org-directory "~/Dropbox/org/notes.org") "Inbox")
-                                   "* %^{Title}\nSource: %u, %c\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?")
-                                  ("s" "Code Snippet" entry (file+headline "~/Dropbox/org/code.org" "Snippet")
-                                   "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC")
-                                  ("r" "Book Reading Task" entry (file+olp "~/Dropbox/org/reading.org" "Reading" "Book")
-                                   "* TODO %^{书名}\n%u\n%a\n" :clock-in t :clock-resume t)
-                                  ("w" "Web Collections" entry (file+headline "~/Dropbox/org/link.org" "Web")
-                                   "* %^{Title}\nSource: %u, %c\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?")
-                                  ("i" "Inbox" entry (file "~/Dropbox/org/inbox.org")
-                                   "* %^{heading} %t %^g\n %?\n")
                                   ("b" "Blog" plain (file ,(concat "~/Dropbox/org/blog/" (format-time-string "%Y-%m-%d.org")))
                                    ,(concat "#+startup: showall\n"
                                             "#+options: toc:nil\n"
@@ -426,7 +372,7 @@
   (add-hook 'bazel-mode-hook (lambda() (add-hook 'before-save-hook #'bazel-format nil t)))
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;All The Icons;;;;;;;;;;;;;;;;;;;;;;
-  (require 'all-the-icons)
+  ;; (require 'all-the-icons)
   (use-package all-the-icons)
   (setq inhibit-compacting-font-caches t)
   (setq neo-theme 'icons)
@@ -434,7 +380,7 @@
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;LaTex;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; LaTex 指定完整文档预览
-  (add-hook 'doc-view-minor-mode-hook 'auto-revert-mode)
+  ;; (add-hook 'doc-view-minor-mode-hook 'auto-revert-mode)
   ;; Acutex
   (setq TeX-view-program-selection '((output-pdf "Skim")))
   (setq TeX-source-correlate-mode t)
@@ -442,18 +388,18 @@
   (setq TeX-source-correlate-method 'synctex)
   (setq TeX-view-program-list '(("PDF Viewer"
                                  "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
-  (setq org-latex-create-formula-image-program 'dvipng)
+  ;; (setq org-latex-create-formula-image-program 'dvipng)
   ;; math formatting convert image to PDF file and HTML file.
-  (setq org-latex-create-formula-image-program 'imagemagick)
-  (setq org-preview-latex-process-alist 'imagemagick)
+  ;; (setq org-latex-create-formula-image-program 'imagemagick)
+  ;; (setq org-preview-latex-process-alist 'imagemagick)
   ;; LaTex 输出PDF高亮代码, hotkey: C-c C-e l l
   ;; pdflatex -shell-escape -interaction nonstopmode <tex-file>
-  (setq org-latex-listings 'minted)
-  (add-to-list 'org-latex-packages-alist '("" "minted"))
+  ;; (setq org-latex-listings 'minted)
+  ;; (add-to-list 'org-latex-packages-alist '("" "minted"))
 
   ;; xah-math-input, SHIFT + SPACE
-  (global-xah-math-input-mode 1)
-  (xah-math-input-mode 1)
+  ;; (global-xah-math-input-mode 1)
+  ;; (xah-math-input-mode 1)
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;C++;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (global-set-key [C-M-tab] 'clang-format-region)
@@ -464,14 +410,14 @@
   (require 'sr-speedbar)
 
   ;; orgmode中英文对齐
-  (when (configuration-layer/layer-usedp 'chinese)
-    (when (and (spacemacs/system-is-mac) window-system)
-      (spacemacs//set-monospaced-font "Monaco" "Hiragino Sans GB" 16 16)))
+  ;; (when (configuration-layer/layer-usedp 'chinese)
+  ;;   (when (and (spacemacs/system-is-mac) window-system)
+  ;;     (spacemacs//set-monospaced-font "Monaco" "Hiragino Sans GB" 16 16)))
   ;; orgmode导出中文
-  (setq org-latex-pdf-process '("xelatex -interaction nonstopmode %f"
-                                "xelatex -interaction nonstopmode %f"))
-  (setq org-latex-default-packages-alist
-        (remove '("AUTO" "inputenc" t) org-latex-default-packages-alist))
+  ;; (setq org-latex-pdf-process '("xelatex -interaction nonstopmode %f"
+  ;;                               "xelatex -interaction nonstopmode %f"))
+  ;; (setq org-latex-default-packages-alist
+  ;;       (remove '("AUTO" "inputenc" t) org-latex-default-packages-alist))
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;LSP;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (add-hook 'hack-local-variables-hook
@@ -483,8 +429,7 @@
     (require 'company-lsp)
     (push 'company-lsp company-backends)
     (add-hook 'after-init-hook 'global-company-mode)
-    ;; (add-to-list 'company-lsp-filter-candidates '(texlab . nil))
-    ;; (add-to-list 'company-lsp-filter-candidates '(digestif . nil))
+    (add-to-list 'company-lsp-filter-candidates '(digestif . nil))
     )
 
   (use-package lsp-mode
@@ -512,11 +457,6 @@
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Dap;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (require 'dap-python)
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Eglot;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (require 'eglot)
-  (add-to-list 'eglot-server-programs '((latex-mode tex-mode context-mode texinfo-mode bibtex-mode)
-                                        . ("/usr/local/bin/texlab")))
-
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Todo;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; (find-file "~/Dropbox/org/week.org")
   (find-file "~/Dropbox/org/todo.org")
@@ -527,12 +467,22 @@
 This is an auto-generated function, do not modify its content directly, use
 Emacs customize menu instead.
 This function is called at the very end of Spacemacs initialization."
-  (custom-set-variables
-   '(safe-local-variable-values
-     (quote
-      ((python-backend . lsp)
-       (javascript-backend . lsp)
-       (go-backend . go-mode)
-       (go-backend . lsp)
-       ))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ansi package-build shut-up epl git commander f dash s))
+ '(safe-local-variable-values
+   '((python-backend . lsp)
+     (go-backend . go-mode)
+     (go-backend . lsp)
+     (rust-mode
+      (rust-backend . lsp)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 )
